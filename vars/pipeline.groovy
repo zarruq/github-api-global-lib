8855665b2faf61fb763b70d8dd60f84aa818ec38
zarruq <muhammadismail1@protonmail.com>
2022-05-16 20:51:58 +0100

    Added vars/pipeline.groovy script

---
+++ vars/pipeline.groovy
@@ -0,0 +1,24 @@
+def call(){
+    pipeline {
+        environment {
+            registryCredential = 'k8sregistry'
+            JAVA_TOOL_OPTIONS = "-Duser.home=/var/maven"
+        }
+        agent {
+            docker {
+                image 'maven:3.8.2-adoptopenjdk-8'
+                args "-v /tmp/maven2:/var/maven/.m2 -e MAVEN_CONFIG=/var/maven/.m2"
+            }
+        }
+        options{
+            timeout(time: 30, unit: "MINUTES")
+        }
+        stages {
+            stage('Build') {
+                steps {
+                    sh 'mvn -B -DskipTests clean package'
+                }
+            }
+        }
+    }
+}
